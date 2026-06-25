c79f85fab2cc442b428df71c6fc65baeee7d26b9
lucasolasz <olaszlucas072@gmail.com>
2026-06-25 09:37:09 -0300

    Inclui endereco ip domestico

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,8 @@ const nextConfig: NextConfig = {
   allowedDevOrigins: [
     "localhost:3000",
     "127.0.0.1:3000",
-    "10.34.6.112:3000", // IP da sua máquina na rede
+    "10.34.6.112:3000",
+    "192.168.1.33:3000", // IP da sua máquina na rede
   ],
 };
 
