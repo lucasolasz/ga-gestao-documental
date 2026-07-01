1b69051f73c4c87f76f925deb2e4bae6fb9c8643
lucasolasz <olaszlucas072@gmail.com>
2026-07-01 00:05:45 -0300

    Inclui as polices da tabela

--- migrations/001_create_familias_documentos.sql
+++ migrations/001_create_familias_documentos.sql
@@ -4,3 +4,37 @@ create table public.familias_documentos (
   created_at timestamp with time zone null default now(),
   constraint familias_pop_pkey primary key (id)
 ) TABLESPACE pg_default;
+
+
+-- SELECT
+create policy "Usuários autenticados podem visualizar"
+on public.familias_documentos
+as permissive
+for select
+to authenticated
+using (true);
+
+-- INSERT
+create policy "Usuários autenticados podem inserir"
+on public.familias_documentos
+as permissive
+for insert
+to authenticated
+with check (true);
+
+-- UPDATE
+create policy "Usuários autenticados podem atualizar"
+on public.familias_documentos
+as permissive
+for update
+to authenticated
+using (true)
+with check (true);
+
+-- DELETE
+create policy "Usuários autenticados podem excluir"
+on public.familias_documentos
+as permissive
+for delete
+to authenticated
+using (true);
